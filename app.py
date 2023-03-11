1f6abe28ce7ccdd7ca19ea5ae1963eb0af69670d
ElemelonWind <c.yang.2023@gmail.com>
2023-03-11 14:27:43 -0500

    upload frontend

--- app.py
+++ app.py
@@ -3,10 +3,20 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def index():
     return render_template('home.html', active={
         'home': True,
         'about': False,
         'tutorial': False,
         'upload': False
+    })
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    return render_template('upload.html', active={
+        'home': False,
+        'about': False,
+        'tutorial': False,
+        'upload': True
     })
