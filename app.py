bad9cbccd5dc27d26b39c0f2a9ad5122e18cf97b
ElemelonWind <c.yang.2023@gmail.com>
2023-03-11 16:24:58 -0500

    m

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+from keras.models import load_model
+from PIL import Image
+import numpy as np
+
+model = load_model('melanoma_inceptionv3.h5')
+
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html', active={
         'home': True,
-        'about': False,
+        'methods': False,
+        'tutorial': False,
+        'upload': False
+    })
+
+@app.route('/methods')
+def methods():
+    return render_template('methods.html', active={
+        'home': False,
+        'methods': True,
         'tutorial': False,
         'upload': False
     })
@@ -15,16 +30,36 @@ def home():
 def tutorial():
     return render_template('tutorial.html', active={
         'home': False,
-        'about': False,
+        'methods': False,
         'tutorial': True,
         'upload': False
     })
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
+    if request.method == 'POST':
+        f = request.files['file']
+
+        img = Image.open(f)
+        img = img.resize((224, 224))
+        img = img.convert('RGB')
+        img.save('static/img/uploaded.jpg')
+        img = np.array(img).reshape(-1, 224, 224, 3)
+
+        prediction = model.predict(img)
+
+        print(prediction)
+
+        return render_template('result.html', prediction=prediction, active={
+            'home': False,
+            'methods': False,
+            'tutorial': False,
+            'upload': True
+        })
+    
     return render_template('upload.html', active={
         'home': False,
-        'about': False,
+        'methods': False,
         'tutorial': False,
         'upload': True
     })
