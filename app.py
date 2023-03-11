f7ddcba6487a476e84f83371699a5fc8b08837c8
elise-yz <elise.zyh@gmail.com>
2023-03-11 13:16:28 -0500

    add tutorial page

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,6 +6,14 @@ def index():
     return render_template('base.html', active={
         'home': True,
         'about': False,
-        'tutorial': False,
+        'tutorial': True,
         'upload': False
     })
+
+@app.route('/tutorial')
+def tutorial():
+    return render_template('tutorial.html')
+
+app.debug = True
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
